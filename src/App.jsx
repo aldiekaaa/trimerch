817e90208ac8f68b37e9e410943e599545b2150c
aldiekaaa <120025106+aldiekaaa@users.noreply.github.com>
2025-01-05 00:49:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Tables from './components/tables'
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+     <div>
+      <Navbar/>
+      <Tables/>
+     </div>
+  )
+}
+
+export default App
